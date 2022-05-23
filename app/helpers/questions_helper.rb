960f85826310584995da8ec6253a909b229d4a46
craig norford <craig.a.norford@gmail.com>
2022-05-23 14:28:22 +0100

    design

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,22 +1,16 @@
 module QuestionsHelper
   def coach_answer(your_message)
-    if your_message.empty? || your_message.casecmp('I am going to work right now!').zero?
-      return ""
-    elsif your_message[-1] == '?'
-      return 'Silly question, get dressed and go to work!'
-    else
-      return 'I don\'t care, get dressed and go to work!'
-    end
+    return 'Great!' if your_message.casecmp('I am going to work right now!').zero?
+
+    return 'Silly question, get dressed and go to work!' if your_message[-1] == '?'
+
+    'I don\'t care, get dressed and go to work!'
   end
 
   def coach_answer_enhanced(your_message)
     answer = coach_answer(your_message)
-    if answer == ''
-      return ''
-    elsif your_message == your_message.upcase
-      return "I can feel your motivation! #{answer}"
-    else
-      return answer
-    end
+    return "I can feel your motivation! #{answer}" if your_message == your_message.upcase
+
+    answer
   end
 end
